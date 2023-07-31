80e372c3bb6071b03a29be13a43343b534e96e53
Dzianis Dashkevich <dskecse@gmail.com>
2023-07-31 13:57:27 +0300

    Implement Abstract Factory pattern

--- src/patterns/01-abstractFactory/index.js
+++ src/patterns/01-abstractFactory/index.js
@@ -5,18 +5,39 @@ export class ErrorNotification {}
 
 export class SuccessControl {
   create (type = '') {
-    // todo: implement logic
+    switch (type) {
+      case "button":
+        return new SuccessButton()
+      case "notification":
+        return new SuccessNotification()
+      default:
+        throw new Error(`SuccessControl type should either be "button" or "notification".`)
+    }
   }
 }
 
 export class ErrorControl {
   create (type = '') {
-    // todo: implement logic
+    switch (type) {
+      case "button":
+        return new ErrorButton()
+      case "notification":
+        return new ErrorNotification()
+      default:
+        throw new Error(`ErrorControl type should either be "button" or "notification".`)
+    }
   }
 }
 
 export default class ControlsFactory {
   getFactory (factoryType = '') {
-    // todo: implement logic
+    switch (factoryType) {
+      case "success":
+        return new SuccessControl()
+      case "error":
+        return new ErrorControl()
+      default:
+        throw new Error(`Factory type should either be "success" or "error".`)
+    }
   }
 }
