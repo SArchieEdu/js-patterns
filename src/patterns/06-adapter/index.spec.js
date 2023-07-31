cfd7acb991397cdf090ac84ba3c71db70862c297
Dzianis Dashkevich <dskecse@gmail.com>
2023-07-31 15:59:44 +0300

    Implement Adapter pattern

--- src/patterns/06-adapter/index.spec.js
+++ src/patterns/06-adapter/index.spec.js
@@ -1,7 +1,7 @@
 import { getArea, Adapter, Square, Rectangle } from './index.js';
 
 describe('patterns/adapter', () => {
-  it('should be abel to calculate the shape area', () => {
+  it('should be able to calculate the shape area', () => {
     expect(getArea(new Rectangle(2, 3))).toBe(6);
     expect(getArea(new Adapter(new Square(10)))).toBe(100);
   });
