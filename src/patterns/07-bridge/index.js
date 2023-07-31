f366bafa83e6d656b1b30233a57261252a973451
Dzianis Dashkevich <dskecse@gmail.com>
2023-07-31 17:23:05 +0300

    Rename Pallet to Palette

--- src/patterns/07-bridge/index.js
+++ src/patterns/07-bridge/index.js
@@ -1,23 +1,23 @@
-export class RedColorsPallet {
+export class RedColorsPalette {
   constructor () {
     this.color = 'red';
   }
 }
 
-export class GreenColorsPallet {
+export class GreenColorsPalette {
   constructor () {
     this.color = 'green';
   }
 }
 
 export class Notification {
-  constructor (name, colorsPallet) {
+  constructor (name, colorsPalette) {
     this.name = name;
-    this.colorsPallet = colorsPallet;
+    this.colorsPalette = colorsPalette;
   }
 
   toString () {
-    return `This is ${this.name} notification with ${this.colorsPallet.color} colors pallet`;
+    return `This is ${this.name} notification with ${this.colorsPalette.color} colors palette`;
   }
 }
 
--- src/patterns/07-bridge/index.spec.js
+++ src/patterns/07-bridge/index.spec.js
@@ -1,15 +1,15 @@
-import { GreenColorsPallet, Notification, RedColorsPallet } from './index.js';
+import { GreenColorsPalette, Notification, RedColorsPalette } from './index.js';
 
 describe('patterns/bridge', () => {
   it('should be able to create "success" notification', () => {
-    const successNotification = new Notification('success', new GreenColorsPallet());
+    const successNotification = new Notification('success', new GreenColorsPalette());
 
-    expect(successNotification.toString()).toBe("This is success notification with green colors pallet");
+    expect(successNotification.toString()).toBe("This is success notification with green colors palette");
   });
 
   it('should be able to create "error" notification', () => {
-    const errorNotification = new Notification('error', new RedColorsPallet());
+    const errorNotification = new Notification('error', new RedColorsPalette());
 
-    expect(errorNotification.toString()).toBe("This is error notification with red colors pallet");
+    expect(errorNotification.toString()).toBe("This is error notification with red colors palette");
   });
 });
