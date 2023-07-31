e90a8566b26ac32f0d13455b2c176e6e559fa815
Dzianis Dashkevich <dskecse@gmail.com>
2023-07-31 14:42:22 +0300

    Implement Factory Method pattern

--- src/patterns/03-factoryMethod/index.js
+++ src/patterns/03-factoryMethod/index.js
@@ -7,17 +7,35 @@ class Input {
 }
 
 export class TextInput extends Input {
-  // todo: implement logic
+  create() {
+    this.element.type = "text";
+    return this;
+  }
 }
 
 export class NumberInput extends Input {
-  // todo: implement logic
+  create() {
+    this.element.type = "number";
+    return this;
+  }
 }
 
 export class EmailInput extends Input {
-  // todo: implement logic
+  create() {
+    this.element.type = "email";
+    return this;
+  }
 }
 
 export const inputFactory = (type = '') => {
-  // todo: implement logic
+  switch (type) {
+    case "text":
+      return new TextInput()
+    case "number":
+      return new NumberInput()
+    case "email":
+      return new EmailInput()
+    default:
+      throw new Error(`Input type should be one of "text", "number" or "email".`)
+  }
 };
