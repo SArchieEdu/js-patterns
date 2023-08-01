660c4c98a8d7895b478486a0fb78c59445fa13d3
Dzianis Dashkevich <dskecse@gmail.com>
2023-08-01 17:17:17 +0300

    Implement Template Method pattern

--- src/patterns/21-templateMethod/index.js
+++ src/patterns/21-templateMethod/index.js
@@ -1,66 +1,50 @@
 class HotDrinksPot {
-  // todo: add implementation
-}
-
-// todo: make refactoring via template method pattern
-export class TeaPot {
   algorithmSteps = {};
 
   constructor() {
     this.prepareRecipe();
   }
 
-  prepareRecipe () {
+  prepareRecipe() {
     this.boilWater();
-    this.brewTea();
+    this.brew();
     this.pourInCup();
-    this.addLemon();
-  }
-
-  boilWater ()  {
-    this.algorithmSteps.boilWater = 'Boiling water';
-  }
-
-  brewTea () {
-    this.algorithmSteps.brewTea = 'Steeping the tea';
-  }
-
-  pourInCup () {
-    this.algorithmSteps.pourInCup = 'Pouring into cup';
-  }
-
-  addLemon () {
-    this.algorithmSteps.addLemon = 'Adding Lemon';
+    this.addCondiments();
   }
 }
 
-export class CoffeePot {
-  algorithmSteps = {};
-
-  constructor() {
-    this.prepareRecipe();
-  }
-
-  prepareRecipe () {
-    this.boilWater();
-    this.brewCoffee();
-    this.pourInCup();
-    this.addSugarAndMilk();
-  }
-
-  boilWater ()  {
+export class TeaPot extends HotDrinksPot {
+  boilWater() {
     this.algorithmSteps.boilWater = 'Boiling water';
   }
 
-  brewCoffee () {
-    this.algorithmSteps.brewCoffee = 'Dripping Coffee through filter';
+  brew() {
+    this.algorithmSteps.brew = 'Steeping the tea';
   }
 
-  pourInCup () {
+  pourInCup() {
     this.algorithmSteps.pourInCup = 'Pouring into cup';
   }
 
-  addSugarAndMilk () {
-    this.algorithmSteps.addSugarAndMilk = 'Adding Sugar and Milk';
+  addCondiments() {
+    this.algorithmSteps.addCondiments = 'Adding Lemon';
+  }
+}
+
+export class CoffeePot extends HotDrinksPot {
+  boilWater() {
+    this.algorithmSteps.boilWater = 'Boiling water';
+  }
+
+  brew() {
+    this.algorithmSteps.brew = 'Dripping Coffee through filter';
+  }
+
+  pourInCup() {
+    this.algorithmSteps.pourInCup = 'Pouring into cup';
+  }
+
+  addCondiments() {
+    this.algorithmSteps.addCondiments = 'Adding Sugar and Milk';
   }
 }
