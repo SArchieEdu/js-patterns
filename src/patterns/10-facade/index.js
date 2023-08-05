cfebbb0359c78a26c0db3065ec20609c321207fa
Artur Mingazhov <arthurming7@gmail.com>
2023-08-05 12:47:03 +0500

    add solution for facade

--- src/patterns/10-facade/index.js
+++ src/patterns/10-facade/index.js
@@ -1,29 +1,42 @@
 export class Game {
-  start () {
-    // todo: add implementation
+  constructor({
+    heroName = "Barbarian",
+    locationName = "darkForest",
+    monsters = ["demon", "demon", "undead"],
+  } = {}) {
+    this.heroName = heroName;
+    this.locationName = locationName;
+    this.monsters = monsters;
+  }
+  start() {
+    this.hero = new Hero(this.heroName);
+    this.location = new Location(this.locationName);
+    this.monsters.forEach((monster) =>
+      this.location.addMonster(new Monster(monster))
+    );
   }
 }
 
 class Location {
   monsters = [];
 
-  constructor(name = '') {
+  constructor(name = "") {
     this.name = name;
   }
 
-  addMonster (monster = {}) {
+  addMonster(monster = {}) {
     this.monsters.push(monster);
   }
 }
 
 export class Hero {
-  constructor(name = '') {
+  constructor(name = "") {
     this.name = name;
   }
 }
 
 export class Monster {
-  constructor(type = '') {
+  constructor(type = "") {
     this.type = type;
   }
 }
