6789bfdba28f6bbd819290ae6adb5bf1b6945bc8
Artur Mingazhov <arthurming7@gmail.com>
2023-08-05 11:18:50 +0500

    add solution for decorator

--- src/patterns/09-decorator/index.js
+++ src/patterns/09-decorator/index.js
@@ -1,20 +1,61 @@
+/* 
+//Base
 export class Milk {
   price = 2;
 
-  // todo: add implementation
+  constructor(coffee = {}) {
+    this.coffee = coffee;
+  }
+
+  getPrice() {
+    return this.coffee.getPrice() + this.price;
+  }
 }
 
 export class Sugar {
   price = 1;
 
-  // todo: add implementation
+  constructor(coffee = {}) {
+    this.coffee = coffee;
+  }
+
+  getPrice() {
+    return this.coffee.getPrice() + this.price;
+  }
 }
 
 export class Coffee {
   price = 5;
 
-  getPrice () {
+  getPrice() {
     return this.price;
+  }
+} */
+
+/* abstract */ class Additives {
+  price = 0;
+
+  constructor(coffee = {}) {
+    this.coffee = coffee;
+  }
+
+  getPrice() {
+    return this.coffee.getPrice() + this.price;
   }
 }
 
+export class Milk extends Additives {
+  price = 2;
+}
+
+export class Sugar extends Additives {
+  price = 1;
+}
+
+export class Coffee {
+  price = 5;
+
+  getPrice() {
+    return this.price;
+  }
+}
