66ebe82065cdaa39ca1c320a028d41933735221a
nozeroex <nozeroex@gmail.com>
2023-08-11 19:52:01 +0300

    Add flyweight solution

--- src/patterns/11-flyweight/index.js
+++ src/patterns/11-flyweight/index.js
@@ -1,5 +1,9 @@
 class Flyweight {
-  // todo: add implementation
+  constructor(name, country, color) {
+    this.name = name;
+    this.country = country;
+    this.color = color;
+  }
 }
 
 export class ProductsStore {
@@ -9,10 +13,18 @@ export class ProductsStore {
   addProduct (productData = {}) {
     const productFlyweight = this.getOrCreateFlyweight(productData.info);
 
-    // todo: add implementation
+    this.products.push({
+      ...productData,
+      info: productFlyweight,
+    });
   }
 
   getOrCreateFlyweight (info = {}) {
-    // todo: add implementation
+    const flyweightKey = `${info.name}${info.country}${info.color}`;
+    if (!this.flyweights[flyweightKey]) {
+      this.flyweights[flyweightKey] = info;
+    }
+    
+    return this.flyweights[flyweightKey];
   }
 }
